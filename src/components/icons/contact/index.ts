04f2b1f27667744e09e5c100e8bf41b79f214403
Chencho34 <armandocrescencio343@gmail.com>
2025-08-11 15:53:25 -0600

    HotFix in home page

---
+++ src/components/icons/contact/index.ts
@@ -0,0 +1,9 @@
+import LinkedinIcon from './LinkedinIcon.astro'
+import MailIcon from './MailIcon.astro'
+import GithubIcon from './GithubIcon.astro'
+
+export {
+  LinkedinIcon,
+  MailIcon,
+  GithubIcon 
+}
