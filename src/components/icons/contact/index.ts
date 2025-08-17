a9f18340df621ccc9d40aedb09ab24a1fea0dfd7
Chencho34 <armandocrescencio343@gmail.com>
2025-08-17 09:33:49 -0600

    NEW: styles

--- src/components/icons/contact/index.ts
+++ src/components/icons/contact/index.ts
@@ -2,10 +2,12 @@ import LinkedinIcon from './LinkedinIcon.astro'
 import MailIcon from './MailIcon.astro'
 import GithubIcon from './GithubIcon.astro'
 import CodeIcon from './CodeIcon.astro'
+import DownloadCV from './DownloadCV.astro'
 
 export {
   LinkedinIcon,
   MailIcon,
   GithubIcon,
-  CodeIcon
+  CodeIcon,
+  DownloadCV
 }
