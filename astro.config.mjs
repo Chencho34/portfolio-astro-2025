10edb4b8d78b1d1fcf68158ab44e496cafe87d30
Jose Armando CR <90371472+Chencho34@users.noreply.github.com>
2025-10-04 19:50:40 -0600

    Revert "Update UI"

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   site: 'https://portfolio-astro-2025-ten.vercel.app/',
   vite: {
+    server: {
+      host: '192.168.1.68'
+    },
     plugins: [tailwindcss()],
   },
 });
