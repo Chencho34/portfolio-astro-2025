4c6c45099c9fe2f2cddb240194c7d465bb66507e
Chencho34 <armandocrescencio343@gmail.com>
2026-03-23 17:39:59 -0600

    Install: siteMap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,23 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://portfolio-astro-2025-ten.vercel.app/',
+
   vite: {
     plugins: [tailwindcss()],
+    build: {
+      cssCodeSplit: true,
+      rollupOptions: {
+        output: {
+          manualChunks: undefined
+        }
+      }
+    }
   },
+
+  integrations: [sitemap()],
 });
